4d1a5b0cc945bda11c44e044394dc9a18f7637bf
Robert Wang <robertwang.tw@gmail.com>
2017-07-25 15:21:21 +0800

    Update azure key and fix null issue if NO faces found.

--- app/src/main/java/com/noodoe/robert/azuresample/MainActivity.kt
+++ app/src/main/java/com/noodoe/robert/azuresample/MainActivity.kt
@@ -25,11 +25,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 FaceAPIFragment.newInstance()
         ).commit()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
